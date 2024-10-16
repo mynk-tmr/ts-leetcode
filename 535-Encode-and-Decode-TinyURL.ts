d2430b4bc980f623016736a6a897bc0526c5c80b
Mayank <146926037+mynk-tmr@users.noreply.github.com>
2024-10-16 23:14:08 +0530

    Update 535-Encode-and-Decode-TinyURL.ts

--- 535-Encode-and-Decode-TinyURL.ts
+++ 535-Encode-and-Decode-TinyURL.ts
@@ -2,7 +2,10 @@ const cache = new Map()
 const prefix = 'http://tinyurl.com/'
 
 function encode(longUrl: string): string {
-    const key = Math.random().toFixed(8);
+    let key = '0';
+    while (cache.has(key)) { //when 50 million entries, a collision is very likely
+        key = Math.random().toFixed(8); //100,000,000 entries
+    }
     cache.set(key, longUrl)
     return prefix + key;
 };
