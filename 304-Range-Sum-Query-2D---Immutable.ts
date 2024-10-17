b209f6b3d2409fff5545df9871a65bb5bc3be2f7
Mayank <146926037+mynk-tmr@users.noreply.github.com>
2024-10-17 23:24:27 +0530

    [304] [Time Beats: 100.00%] [Memory Beats: 36.05%] - LeetPush

---
+++ 304-Range-Sum-Query-2D---Immutable.ts
@@ -0,0 +1,24 @@
+class NumMatrix {
+    dp: number[][] = [] //sum of rectangle upto (i)(j)
+    constructor(matrix: number[][]) {
+        const R = matrix.length, C = matrix[0].length;
+        this.dp[0] = Array(C + 1).fill(0); //
+        for (let i = 1; i <= R; i++) {
+            this.dp[i] = Array(C + 1).fill(0);
+            for (let j = 1; j <= C; j++) {
+                this.dp[i][j] =
+                    this.dp[i - 1][j] +     //prev Row
+                    this.dp[i][j - 1] -       //prev Col
+                    this.dp[i - 1][j - 1] +     //diagonal
+                    matrix[i - 1][j - 1]
+
+            }
+        }
+    }
+
+    sumRegion(row1: number, col1: number, row2: number, col2: number): number {
+        const R = row2 + 1, C = col2 + 1, dp = this.dp
+        //reverse the assigning in constructor
+        return dp[R][C] - dp[row1][C] - dp[R][col1] + dp[row1][col1]
+    }
+}
