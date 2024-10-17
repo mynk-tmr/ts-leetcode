94286d46ce1b69cecd165e99ad89e1541c0e2d63
Mayank <146926037+mynk-tmr@users.noreply.github.com>
2024-10-17 20:39:54 +0530

    Update 307-Range-Sum-Query---Mutable.ts

--- 307-Range-Sum-Query---Mutable.ts
+++ 307-Range-Sum-Query---Mutable.ts
@@ -1,23 +1,38 @@
 class NumArray {
-    update(index: number, val: number): void {
-        let diff = val - this.nums[index];
-        for(let i=index; i<this.nums.length; i++)
-            this.run_sum[i] += diff;
-        
-        this.nums[index] = val;
-
+    tree: number[] = [] //use a Segment tree; root is at tree[1], for simplifying indexing logic
+    n = 0;
+    constructor(nums: number[]) {
+        this.n = nums.length;
+        for (let i = 0; i < this.n; i++) //store leaf nodes -> nums
+            this.tree[this.n + i] = nums[i]
+        for (let i = this.n - 1; i > 0; i--) //store internal nodes -> partial sums
+            this.tree[i] = this.tree[2 * i] + this.tree[2 * i + 1]
     }
 
-    run_sum: number[] = []
-    constructor(private nums: number[]) {
-        let sum = 0;
-        for (let i = 0; i < this.nums.length; i++) {
-            sum += this.nums[i]
-            this.run_sum[i] = sum;
+    update(index: number, val: number): void {
+        let i = index + this.n; //adjust for N
+        this.tree[i] = val;
+        while (i > 1) {
+            i = Math.floor(i / 2) //go to parent for updations
+            this.tree[i] = this.tree[2 * i] + this.tree[2 * i + 1]
         }
     }
 
     sumRange(left: number, right: number): number {
-        return this.run_sum[right] - this.run_sum[left] + this.nums[left]
+        let A = left + this.n, B = right + this.n + 1;  //+1 bcoz inclusive range
+        let sum = 0;
+        while (A < B) {
+            if (A % 2 === 1) { //odd, it's right child, include it
+                sum += this.tree[A]
+                A++
+            }
+            if (B % 2 === 1) { //odd, it's left child, go back
+                B--;
+                sum += this.tree[B]
+            }
+            A = Math.floor(A / 2)
+            B = Math.floor(B / 2)
+        }
+        return sum
     }
 }
