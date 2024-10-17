cbc762b1d2560c7db999243c25cec987c3914c8d
Mayank <146926037+mynk-tmr@users.noreply.github.com>
2024-10-17 19:08:27 +0530

    Update 560-Subarray-Sum-Equals-K.ts

--- 560-Subarray-Sum-Equals-K.ts
+++ 560-Subarray-Sum-Equals-K.ts
@@ -12,5 +12,18 @@ function subarraySum(nums: number[], k: number): number {
 
 //use a map to store running sums upto each index
 //if for an i, somewhere a running sum of sum-k is present
-//we know from @ to i, sum is k
-//update total
+//we know from @ to i, sum is k. 
+
+function getLongestSubarraySumtoK(arr, k) {
+    let map = new Map(), csum = 0, maxlen = 0;
+    for(let i=0; i<arr.length; i++) {
+       csum+= arr[i];
+       let pre = map.get(csum - k);
+       if(pre !== undefined) maxlen = Math.max(maxlen, i - pre)
+       if(!map.has(csum)) map.set(csum, i)
+       if(csum === k) maxlen = Math.max(maxlen, i+1)
+    }
+    return maxlen
+}
+
+//update indexmap only once (for the farthest occurence of sum)
