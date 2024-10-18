a7ae90b6c2f4af2d1b9c39774401ab1150a3c2b0
Mayank <146926037+mynk-tmr@users.noreply.github.com>
2024-10-18 05:38:24 +0530

    Update 739-Daily-Temperatures.ts

--- 739-Daily-Temperatures.ts
+++ 739-Daily-Temperatures.ts
@@ -1,12 +1,12 @@
 function dailyTemperatures(T: number[]): number[] {
-    const stack = [] as number[], map = Array(T.length).fill(0)
+    const stack = [] as number[], dist = Array(T.length).fill(0)
     const top = () => T[stack.at(-1)]
     T.forEach((val, i) => {
         while (stack.length && top() < val) {
             const idx = stack.pop()
-            map[idx] = i - idx;
+            dist[idx] = i - idx;
         }
         stack.push(i)
     })
-    return T.map((_, i) => map[i])
+    return T.map((_, i) => dist[i])
 };
